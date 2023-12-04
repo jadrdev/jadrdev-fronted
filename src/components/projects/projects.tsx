e25dc1fbaca5656dd294033e838d35013640a94c
Joshua A. Díaz Robayna <jadrdev@gmail.com>
2023-12-04 17:19:09 +0000

    Shows Projects

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import fonto from "../../../src/img/fonto.webp";
 import Link from "next/link";
 import { Strings } from "@/constants/string";
 import { api } from "@/api/api";
@@ -36,11 +35,11 @@ export default function Projects() {
 
   return (
     <section>
-      <div className="max-w-7xl mx-auto h-28">
+      <div className="max-w-7xl mx-auto h-28 mb-6">
         <h2 className="text-center font-header text-4xl font-semibold uppercase sm:text-5xl lg:text-5xl">
           {Strings.Project}
         </h2>
-        <h4 className="font-header text-xl font-medium py-2 sm:text-2xl text-center lg:text-3xl">
+        <h4 className="font-header text-xl font-medium py-2  sm:text-2xl text-center lg:text-3xl">
           {Strings.ProjectDescription}
         </h4>
       </div>
@@ -49,17 +48,18 @@ export default function Projects() {
         <div className="dark:bg-gray-900">
           <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 py-18 pb-215">
             {/* Single card */}
-            
+            {projects.map((project) => (
+              <div key={project.id} className="md:w-1/1 p-4">
               <Card
                 isFooterBlurred
-                className="w-full h-[300px] col-span-12 sm:col-12"
+                className="w-full h-[300px] col-span-12 sm:col-auto"
               >
                 <CardHeader className="absolute z-10 top-1 flex-col items-start">
                   <p className="text-tiny text-black/60 uppercase font-bold">
-                    {projects[0].tags.map((tag, index) => (
+                    {project.tags.map((tag, index) => (
                       <span key={tag.id}>
                         {tag.name}
-                        {index < projects[0].tags.length - 1 && ", "}
+                        {index < project.tags.length - 1 && ", "}
                       </span>
                     ))}
                   </p>
@@ -68,13 +68,13 @@ export default function Projects() {
                   removeWrapper
                   alt="Relaxing app background"
                   className="z-0 w-full h-full object-cover"
-                  src={projects[0].feature_image}
+                  src={project.feature_image}
                 />
                 <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                   <div className="flex flex-grow gap-2 items-center">
                     <div className="flex flex-col">
                       <p className="text-tiny text-white/60">
-                        {projects[0].title}
+                        {project.title}
                       </p>
                     </div>
                   </div>
@@ -83,81 +83,8 @@ export default function Projects() {
                   </Button>
                 </CardFooter>
               </Card>
-            
-            <Card
-              isFooterBlurred
-              className="w-full h-[300px] col-span-12 sm:col-auto"
-            >
-              <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                <p className="text-tiny text-white/60 uppercase font-bold">
-                  Your day your way
-                </p>
-                <h4 className="text-white/90 font-medium text-xl">
-                  Your checklist for better sleep
-                </h4>
-              </CardHeader>
-              <Image
-                removeWrapper
-                alt="Relaxing app background"
-                className="z-0 w-full h-full object-cover"
-                src="/images/card-example-5.jpeg"
-              />
-              <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-                <div className="flex flex-grow gap-2 items-center">
-                  <Image
-                    alt="Breathing app icon"
-                    className="rounded-full w-10 h-11 bg-black"
-                    src="/images/breathing-app-icon.jpeg"
-                  />
-                  <div className="flex flex-col">
-                    <p className="text-tiny text-white/60">Breathing App</p>
-                    <p className="text-tiny text-white/60">
-                      Get a good night's sleep.
-                    </p>
-                  </div>
-                </div>
-                <Button radius="full" size="sm">
-                  Get App
-                </Button>
-              </CardFooter>
-            </Card>
-            <Card
-              isFooterBlurred
-              className="w-full h-[300px] col-span-12 sm:col-auto"
-            >
-              <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                <p className="text-tiny text-white/60 uppercase font-bold">
-                  Your day your way
-                </p>
-                <h4 className="text-white/90 font-medium text-xl">
-                  Your checklist for better sleep
-                </h4>
-              </CardHeader>
-              <Image
-                removeWrapper
-                alt="Relaxing app background"
-                className="z-0 w-full h-full object-cover"
-                src="/images/card-example-5.jpeg"
-              />
-              <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-                <div className="flex flex-grow gap-2 items-center">
-                  <Image
-                    alt="Breathing app icon"
-                    className="rounded-full w-10 h-11 bg-black"
-                    src="/images/breathing-app-icon.jpeg"
-                  />
-                  <div className="flex flex-col">
-                    <p className="text-tiny text-white/60">Breathing App</p>
-                    <p className="text-tiny text-white/60">
-                      Get a good night's sleep.
-                    </p>
-                  </div>
-                </div>
-                <Button radius="full" size="sm">
-                  Get App
-                </Button>
-              </CardFooter>
-            </Card>
+              </div>
+            ))}
           </div>
         </div>
       ) : (
