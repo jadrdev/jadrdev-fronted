e9c0ac5e69b9b087c59d6c584c6be584eebc60ea
Joshua A. Díaz Robayna <jdiarob@saosl.com>
2024-11-14 18:26:47 +0000

    Elimina los metadatos de Open Graph y Twitter del componente de la página del blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,43 +5,8 @@ import Menu from "@/components/header/menu";
 import Image from "next/image";
 import Avatar from "../../../img/avatar.webp";
 import { useEffect, useState } from "react";
-import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: {
-    template: '%s | JadrDev - Joshua A. Díaz Robayna',
-    default: 'JadrDev - Software Mobile & Frontend Developer ', // a default is required when creating a template
-  },
-  openGraph: {
-    type: 'website',
-    locale: 'es_ES',
-    url: 'https://jadrdev.com',
-    siteName: 'JadrDev',
-    title: 'JadrDev - Software Mobile & Frontend Developer',
-    images: [
-      {
-        url: 'https://www.jadrdev.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.c4c85032.webp',
-        width: 1200,
-        height: 630,
-        alt: 'JadrDev - Software Mobile & Frontend Developer',
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "JadrDev - Software Mobile & Frontend Developer",
-    description: "Software Mobile & Frontend Developer",
-    creator: "@jadrdev",
-    images: [
-      {
-        url: "https://www.jadrdev.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.c4c85032.webp",
-        width: 1200,
-        height: 630,
-        alt: "JadrDev - Software Mobile & Frontend Developer",
-      },
-    ],
-  },
-}
+
 
 type PageProps = {
   params: {
