866526a6410e69a10152d0b1a238796faccc8fbf
Joshua A. Díaz Robayna <jdiarob@saosl.com>
2024-11-30 17:55:01 +0000

    Añade el icono de PHP en el componente Works, actualiza los títulos de los desarrolladores y agrega un nuevo logo SVG de Ionic

--- src/components/works/works.tsx
+++ src/components/works/works.tsx
@@ -6,7 +6,7 @@ import Velocitech from "../../../src/img/logo-light-c.svg";
 import keapps from "../../../src/img/keapps-logo__negative.svg";
 import sao from "../../../src/img/Logotipo-SAO-e1647363525963.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngular, faReact, faDocker, faJs, faDartLang, faWordpress, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
+import { faAngular, faReact, faDocker, faJs, faDartLang, faWordpress, faMicrosoft, faPhp } from '@fortawesome/free-brands-svg-icons';
 import { faFlutter } from "@fortawesome/free-brands-svg-icons/faFlutter";
 
 
@@ -52,17 +52,33 @@ const Experience = () => {
                         <FontAwesomeIcon icon={faAngular} size="2x" style={
                             {marginRight: "10px"}
                         }/>
-                        <FontAwesomeIcon icon={faReact} size="2x" style={
-                            {marginRight: "10px"}
-                        } />
-                        <FontAwesomeIcon icon={faDocker} size="2x" style={
+                        <svg xmlns="http://www.w3.org/2000/svg" style={
+                          {
+                            fill: "#9ca3af",
+                            stroke: "#9ca3af",
+                            width: "2em",
+                            height: "2em",
+                            marginRight: "10px"
+                          }
+                        } 
+                        className="ionicon" 
+                        viewBox="0 0 512 512">
+                          <path d="M256 153.9A102.1 102.1 0 10358.1 256 102.23 102.23 0 00256 153.9z"/>
+                          <circle cx="402.59" cy="116.45" r="46.52"/>
+                          <path d="M459.86 163.2l-1.95-4.28-3.11 3.52a70 70 0 01-28.06 19.32l-3 1.1 1.22 2.93A181.43 181.43 0 01439 256c0 100.92-82.1 183-183 183S73 356.92 73 256 155.08 73 256 73a180.94 180.94 0 0178.43 17.7l2.87 1.3 1.25-2.92A70.19 70.19 0 01359.21 62l3.67-2.93-4.17-2.07A221.61 221.61 0 00256 32C132.49 32 32 132.49 32 256s100.49 224 224 224 224-100.49 224-224a222.19 222.19 0 00-20.14-92.8z"/>
+                          </svg>
+                          <FontAwesomeIcon icon={faJs} size="2x" style={
                             {marginRight: "10px"}
                         }/>
+
+                        <FontAwesomeIcon icon={faPhp} size="2x" style={
+                            {marginRight: "10px"}
+                        }/>
+
                         <FontAwesomeIcon icon={faWordpress} size="2x" style={
                             {marginRight: "10px"}
                         }/>
                         </div>
-                       
                     </div>
                 </div>
               </div>
@@ -91,7 +107,7 @@ const Experience = () => {
                 <div className="mt-1 flex">
                     <div className="ml-4">
                         <h4 className="text-lg leading-6 font-semibold text-white">
-                        Mobile Developer
+                        Middle Mobile Developer
                         </h4>
                         <p className="mt-2 text-base leading-6 text-white">
                         {Strings.TechnologyUsage}
@@ -139,7 +155,7 @@ const Experience = () => {
                 <div className="mt-1 flex">
                     <div className="ml-4">
                         <h4 className="text-lg leading-6 font-semibold text-white">
-                        Software Developer
+                        Middle Software Developer
                         </h4>
                         <p className="mt-2 text-base leading-6 text-white">
                         {Strings.TechnologyUsage}
@@ -184,7 +200,7 @@ const Experience = () => {
                 <div className="mt-1 flex">
                     <div className="ml-4">
                         <h4 className="text-lg leading-6 font-semibold text-white">
-                        Frontend Web Developer
+                        Junior Frontend Developer
                         </h4>
                         <p className="mt-2 text-base leading-6  text-white">
                         {Strings.TechnologyUsage}
